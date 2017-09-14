895fb925bf8fd873745a7190ccddf12260a36941
Keith Getty <keithgetty@yahoo.com>
2017-09-14 22:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
 }
-
+l
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
@@ -21,5 +21,17 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  return (...kittens, name);
+  return [...kittens, name];
 }
+
+function prependKitten(name) {
+   return [name, ...kittens];
+ }
+
+ function removeLastKitten() {
+   return kittens.slice(0, kittens.length - 1);
+ }
+
+ function removeFirstKitten() {
+   return kittens.slice(1);
+ }
